80a11650f745a8120187454d6624a190755d3288
Harrypoggers25 <mfa.g4e@gmail.com>
2026-06-28 23:38:35 +0800

    Add: rolesOrDeviceParamOwner access control handler

--- src/middlewares/access-control.middleware.ts
+++ src/middlewares/access-control.middleware.ts
@@ -1,5 +1,5 @@
 // MIDDLEWARE
-import { Device } from "../configs/db.config";
+import { Device, DeviceParam } from "../configs/db.config";
 import { stringifyJson } from "../helpers/json.helper";
 import { getPayload } from "./authorization.middleware";
 
@@ -54,6 +54,38 @@ namespace AccessControl {
             }
             const { user_id, user_role } = user;
 
+            const device = await Device.find({ where: { d_id, user_id } });
+            if (!device) {
+                res.status(403);
+                throw new Error(`Forbidden access. Unable to find device ${stringifyJson({ user_id })}`);
+            }
+            if (!roles.includes(user_role) && !device.length) {
+                res.status(403);
+                throw new Error('Forbidden access. Admin role or Account owner access required');
+            }
+
+            next();
+        });
+    }
+
+    export const rolesOrDeviceParamOwner = (roles: Array<string>) => {
+        return Route.asyncHandler(async (req, res, next) => {
+            const user = getPayload(req);
+            const dp_id = +req.params.dp_id;
+
+            if (!user) {
+                res.status(403);
+                throw new Error('Forbidden access. No session available');
+            }
+            const { user_id, user_role } = user;
+
+            const deviceParam = await DeviceParam.findByPk(dp_id)
+            if (!deviceParam) {
+                res.status(403);
+                throw new Error(`Forbidden access. Unable to find device param [${dp_id}]`);
+            }
+
+            const d_id = deviceParam.d_id;
             const device = await Device.find({ where: { d_id, user_id } });
             if (!device) {
                 res.status(403);
