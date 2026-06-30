eb8cf4285afba7140d9b58a2449dd181a3f1d91e
Harrypoggers25 <mfa.g4e@gmail.com>
2026-07-01 04:27:09 +0800

    Add: Company router

---
+++ src/routers/company.router.ts
@@ -0,0 +1,39 @@
+// CONTROLLERS
+import {
+    createCompanyHandler,
+    findAllCompanyHandler,
+    findCompanyHandler,
+    updateCompanyHandler,
+    deleteCompanyHandler,
+} from '../controllers/company.controller';
+import { createUserByCompanyHandler, findAllUserByCompanyHandler } from '../controllers/user.controller';
+import { createSiteByCompanyHandler, findAllSiteByCompanyHandler } from '../controllers/site.controller';
+import { findAllDeviceByCompanyHandler } from '../controllers/device.controller';
+
+// MIDDLEWARES
+import AccessControl from '../middlewares/access-control.middleware';
+import Authorize from '../middlewares/authorization.middleware';
+
+// MODULES
+import { Router } from 'express';
+
+const companyRouter = Router();
+companyRouter.use(Authorize.accesstoken);
+
+companyRouter.route('/')
+    .post(AccessControl.roles(), createCompanyHandler)
+    .get(AccessControl.roles(), findAllCompanyHandler);
+companyRouter.route('/:comp_id')
+    .get(AccessControl.companyAccount(['admin', 'user']), findCompanyHandler)
+    .patch(AccessControl.companyAccount(['admin']), updateCompanyHandler)
+    .delete(AccessControl.companyAccount(['admin']), deleteCompanyHandler);
+companyRouter.route('/:comp_id/users')
+    .post(AccessControl.companyAccount(['admin']), createUserByCompanyHandler)
+    .get(AccessControl.companyAccount(['admin']), findAllUserByCompanyHandler);
+companyRouter.route('/:comp_id/sites')
+    .post(AccessControl.companyAccount(['admin']), createSiteByCompanyHandler)
+    .get(AccessControl.companyAccount(['admin', 'user']), findAllSiteByCompanyHandler);
+companyRouter.route('/:comp_id/devices')
+    .get(AccessControl.companyAccount(), findAllDeviceByCompanyHandler);
+
+export default companyRouter;
