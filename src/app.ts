aa0c92d5b2d7f391a3be6a71fcf590d3386edc4f
Harrypoggers25 <mfa.g4e@gmail.com>
2026-06-30 15:51:06 +0800

    Build 2

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import cookieParser from 'cookie-parser';
 
 App.listen({
     port: env.PORT,
-    version: '1.0.0 Build 1',
+    version: '1.0.0 Build 2',
     cors: [env.ORIGIN_URL],
     beforeListen: async (app) => {
         app.use(cookieParser());
