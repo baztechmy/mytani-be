b9113f609498fad7e5fbc87bd691d252ba53e4cd
Harrypoggers25 <mfa.g4e@gmail.com>
2026-07-01 13:01:59 +0800

    Add: Company and site router activation

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,7 +3,9 @@ import { Router } from "express";
 
 // ROUTERS
 import authenticationRouter from "./authentication.router";
+import companyRouter from "./company.router";
 import userRouter from "./user.router";
+import siteRouter from "./site.router";
 import deviceRouter from "./device.router";
 import deviceRelayRouter from "./device-relay.router";
 import deviceParamRouter from "./device-param.router";
@@ -12,7 +14,9 @@ import deviceDataRouter from "./device-data.router";
 const router = Router();
 
 router.use('/auth', authenticationRouter)
+router.use('/api/companies', companyRouter);
 router.use('/api/users', userRouter);
+router.use('/api/sites', siteRouter);
 router.use('/api/devices', deviceRouter);
 router.use('/api/device-relays', deviceRelayRouter);
 router.use('/api/device-params', deviceParamRouter);
