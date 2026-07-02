790267a36e7d1b83030d40893e40cfda36d3ce53
Harrypoggers25 <mfa.g4e@gmail.com>
2026-07-02 16:50:14 +0800

    Update: Implemented Message helper in site controller

--- src/controllers/site.controller.ts
+++ src/controllers/site.controller.ts
@@ -1,9 +1,6 @@
 // CONFIGS
 import { db, Site } from "../configs/db.config";
 
-// HELPERS
-import { stringifyJson } from "../helpers/json.helper";
-
 // MODULES
 import Route from "@harrypoggers25/route";
 
@@ -12,6 +9,7 @@ import { getPayload } from "../middlewares/authorization.middleware";
 
 // SERVICES
 import { createUserActivityLog } from "../services/user-activity-log.service";
+import Message from "../helpers/message.helper";
 
 export const createSiteByCompanyHandler = Route.asyncHandler(async (req, res) => {
     const date = new Date();
@@ -23,13 +21,17 @@ export const createSiteByCompanyHandler = Route.asyncHandler(async (req, res) =>
     const transaction = await db.transaction({ rollbackOnError: true });
 
     const site = await Site.create({ site_name, site_location, comp_id, created_at, updated_at }, { transaction });
-    if (!site) throw new Error(`Failed to create site by company ${stringifyJson({ comp_id })}`);
+    if (!site) throw new Error(Message.failed(['create', 'site by company', { comp_id }]));
 
-    const ual = await createUserActivityLog(
-        { ual_type: 'SITE_CREATE', ual_activity: `Created new site with site_id = '${site.site_id}'`, user_id: payload.user_id },
-        transaction
-    );
-    if (!ual) throw new Error(`Failed to create site by company ${stringifyJson({ comp_id })}. Unable to create new user activity log`);
+    const { site_id } = site;
+    const ual = await createUserActivityLog({
+        ual_type: 'SITE_CREATE',
+        ual_activity: Message.success(['create', 'new site', site_id]),
+        user_id: payload.user_id
+    }, transaction);
+    if (!ual) throw new Error(Message.failed(['create', 'site by company', { comp_id }], {
+        causer: ['create', 'new user activity log']
+    }));
 
     await transaction.commit();
     res.status(201).json(site);
@@ -38,14 +40,14 @@ export const createSiteByCompanyHandler = Route.asyncHandler(async (req, res) =>
 export const findSiteHandler = Route.asyncHandler(async (req, res) => {
     const site_id = +req.params.site_id;
     const site = await Site.findByPk(+site_id);
-    if (!site) throw new Error(`Failed to find site [${site_id}]`);
+    if (!site) throw new Error(Message.failed(['find', 'site', site_id]));
 
     res.status(200).json(site);
 });
 
 export const findAllSiteHandler = Route.asyncHandler(async (_, res) => {
     const sites = await Site.find();
-    if (!sites) throw new Error('Failed to find all sites');
+    if (!sites) throw new Error(Message.failed(['find', 'all sites']));
 
     res.status(200).json(sites);
 });
@@ -53,7 +55,7 @@ export const findAllSiteHandler = Route.asyncHandler(async (_, res) => {
 export const findAllSiteByCompanyHandler = Route.asyncHandler(async (req, res) => {
     const comp_id = +req.params.comp_id;
     const sites = await Site.find({ where: { comp_id } });
-    if (!sites) throw new Error(`Failed to find all sites by company ${stringifyJson({ comp_id })}`);
+    if (!sites) throw new Error(Message.failed(['find', 'all sites by company', { comp_id }]));
 
     res.status(200).json(sites);
 });
@@ -66,13 +68,16 @@ export const updateSiteHandler = Route.asyncHandler(async (req, res) => {
     const transaction = await db.transaction({ rollbackOnError: true });
 
     const site = await Site.updateByPk(site_id, { site_name, site_location, updated_at }, { transaction });
-    if (!site) throw new Error(`Failed to update site [${site_id}]`);
+    if (!site) throw new Error(Message.failed(['update', 'site', site_id]));
 
-    const ual = await createUserActivityLog(
-        { ual_type: 'SITE_UPDATE', ual_activity: `Updated site with site_id = '${site_id}'`, user_id: payload.user_id },
-        transaction
-    );
-    if (!ual) throw new Error(`Failed to update site [${site_id}]. Unable to create new user activity log`);
+    const ual = await createUserActivityLog({
+        ual_type: 'SITE_UPDATE',
+        ual_activity: Message.success(['update', 'site', site_id]),
+        user_id: payload.user_id
+    }, transaction);
+    if (!ual) throw new Error(Message.failed(['update', 'site', site_id], {
+        causer: ['create', 'new user activity log']
+    }));
 
     await transaction.commit();
     res.status(200).json(site);
@@ -84,13 +89,16 @@ export const deleteSiteHandler = Route.asyncHandler(async (req, res) => {
     const transaction = await db.transaction({ rollbackOnError: true });
 
     const site = await Site.deleteByPk(site_id, { transaction });
-    if (!site) throw new Error(`Failed to delete site [${site_id}]`);
+    if (!site) throw new Error(Message.failed(['delete', 'site', site_id]));
 
-    const ual = await createUserActivityLog(
-        { ual_type: 'SITE_DELETE', ual_activity: `Deleted site with site_id = '${site_id}'`, user_id: payload.user_id },
-        transaction
-    );
-    if (!ual) throw new Error(`Failed to delete site [${site_id}]. Unable to create new user activity log`);
+    const ual = await createUserActivityLog({
+        ual_type: 'SITE_DELETE',
+        ual_activity: Message.success(['delete', 'site', site_id]),
+        user_id: payload.user_id
+    }, transaction);
+    if (!ual) throw new Error(Message.failed(['delete', 'site', site_id], {
+        causer: ['create', 'new user activity log']
+    }));
 
     await transaction.commit();
     res.status(200).json(site);
