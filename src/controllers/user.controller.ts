065211a6ede306976f6ef2a4e254072ae1b12d64
Harrypoggers25 <mfa.g4e@gmail.com>
2026-07-02 16:07:22 +0800

    Update: Implemented Message helper in user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,6 @@
 // CONFIGS
 import { db, User, UserSecret } from "../configs/db.config";
 
-// HELPERS
-import { stringifyJson } from "../helpers/json.helper";
-
 // MODULES
 import Route from "@harrypoggers25/route";
 import { hashSync } from "bcrypt-ts";
@@ -14,6 +11,7 @@ import { getPayload } from "../middlewares/authorization.middleware";
 // SERVICES
 import { createUserActivityLog } from "../services/user-activity-log.service";
 import { Roles } from "../middlewares/access-control.middleware";
+import Message from "../helpers/message.helper";
 
 export const createUserByCompanyHandler = Route.asyncHandler(async (req, res) => {
     const date = new Date();
@@ -26,12 +24,16 @@ export const createUserByCompanyHandler = Route.asyncHandler(async (req, res) =>
 
     if (!user_email) {
         res.status(400);
-        throw new Error(`Failed to create new user by company ${stringifyJson({ comp_id })}. User email is required`);
+        throw new Error(Message.failed(['create', 'new user by company', { comp_id }], {
+            subMessage: 'user_email is required'
+        }));
     }
 
     if (!user_password) {
         res.status(400);
-        throw new Error(`Failed to create new user by company ${stringifyJson({ comp_id })}. User password is required`);
+        throw new Error(Message.failed(['create', 'new user by company', { comp_id }], {
+            subMessage: 'user_password is required'
+        }));
     }
     user_password = hashSync(user_password, 10);
 
@@ -41,16 +43,25 @@ export const createUserByCompanyHandler = Route.asyncHandler(async (req, res) =>
         { user_name, user_email, user_phone, user_role, comp_id, created_at, updated_at, created_by },
         { transaction }
     );
-    if (!user) throw new Error(`Failed to create new user by company ${stringifyJson({ comp_id })}. Internal error`);
+    if (!user) throw new Error(Message.failed(['create', 'new user by company', { comp_id }]));
 
+    const { user_id } = user;
     const userSecret = await UserSecret.create({ user_password, user_id: user.user_id }, { transaction });
-    if (!userSecret) throw new Error(`Failed to create new user by company ${stringifyJson({ comp_id })}. Unable to create user password`);
+    if (!userSecret) throw new Error(Message.failed(['create', 'new user by company', { comp_id }], {
+        causer: ['create', 'user password']
+    }));
 
-    const ual = await createUserActivityLog(
-        { ual_type: 'USER_CREATE', ual_activity: `Created user account with user_id = '${user.user_id}'`, ual_date: date, user_id: payload.user_id },
+    const ual = await createUserActivityLog({
+        ual_type: 'USER_CREATE',
+        ual_activity: Message.success(['create', 'user account', user_id]),
+        ual_date: date,
+        user_id: payload.user_id
+    },
         transaction
     );
-    if (!ual) throw new Error(`Failed to create new user by company ${stringifyJson({ comp_id })}. Unable to create new user activity log`);
+    if (!ual) throw new Error(Message.failed(['create', 'new user by company', { comp_id }], {
+        causer: ['create', 'new user activity log']
+    }));
 
     await transaction.commit();
     res.status(201).json(user);
@@ -59,14 +70,14 @@ export const createUserByCompanyHandler = Route.asyncHandler(async (req, res) =>
 export const findUserHandler = Route.asyncHandler(async (req, res) => {
     const user_id = +req.params.user_id;
     const user = await User.findByPk(user_id);
-    if (!user) throw new Error(`Failed to find user [${user_id}]`);
+    if (!user) throw new Error(Message.failed(['find', 'user', user_id]));
 
     res.status(200).json(user);
 });
 
 export const findAllUserHandler = Route.asyncHandler(async (_, res) => {
     const users = await User.find();
-    if (!users) throw new Error(`Failed to find all users`);
+    if (!users) throw new Error(Message.failed(['find', 'all users']));
 
     res.status(200).json(users);
 });
@@ -74,7 +85,7 @@ export const findAllUserHandler = Route.asyncHandler(async (_, res) => {
 export const findAllUserByCompanyHandler = Route.asyncHandler(async (req, res) => {
     const comp_id = +req.params.comp_id;
     const users = await User.find({ where: { comp_id } });
-    if (!users) throw new Error(`Failed to find all users by company ${stringifyJson({ comp_id })}`);
+    if (!users) throw new Error(Message.failed(['find', 'all users by company', { comp_id }]));
 
     res.status(200).json(users);
 });
@@ -88,25 +99,33 @@ export const updateUserHandler = Route.asyncHandler(async (req, res) => {
 
     if (user_password) {
         const userSecret = await UserSecret.update({ user_password: hashSync(user_password, 10) }, { where: { user_id }, transaction });
-        if (!userSecret) throw new Error(`Failed to update user [${user_id}].Invalid user password`);
+        if (!userSecret) throw new Error(Message.failed(['update', 'user', user_id], {
+            subMessage: 'Invalid user password'
+        }));
     }
 
     const roles: Array<Roles> = ['admin', 'user'];
     if (user_role && (typeof user_role !== 'string' || !roles.some(role => user_role.toLowerCase() === role))) {
         await transaction.rollback()
         res.status(400);
-        throw new Error(`Failed to update user [${user_id}]. Invalid user role`);
+        throw new Error(Message.failed(['update', 'user', user_id], {
+            subMessage: 'Invalid user role'
+        }));
     }
 
     const user = await User.updateByPk(user_id, { user_name, user_email, user_phone, user_role, updated_at }, { transaction });
-    if (!user) throw new Error(`Failed to update user [${user_id}]`);
+    if (!user) throw new Error(Message.failed(['update', 'user', user_id]));
 
     const payload = getPayload(req);
-    const ual = await createUserActivityLog(
-        { ual_type: 'USER_UPDATE', ual_activity: `Updated user account with user_id = '${user_id}'`, ual_date: updated_at, user_id: payload.user_id },
-        transaction
-    );
-    if (!ual) throw new Error(`Failed to update user [${user_id}]. Unable to create new user activity log`);
+    const ual = await createUserActivityLog({
+        ual_type: 'USER_UPDATE',
+        ual_activity: Message.success(['update', 'user account', user_id]),
+        ual_date: updated_at,
+        user_id: payload.user_id
+    }, transaction);
+    if (!ual) throw new Error(Message.failed(['update', 'user', user_id], {
+        causer: ['create', 'new user activity log']
+    }));
 
     await transaction.commit();
     res.status(200).json(user);
@@ -117,14 +136,17 @@ export const deleteUserHandler = Route.asyncHandler(async (req, res) => {
     const transaction = await db.transaction({ rollbackOnError: true });
 
     const user = await User.deleteByPk(user_id, { transaction });
-    if (!user) throw new Error(`Failed to delete user [${user_id}]`);
+    if (!user) throw new Error(Message.failed(['delete', 'user', user_id]));
 
     const payload = getPayload(req);
-    const ual = await createUserActivityLog(
-        { ual_type: 'USER_DELETE', ual_activity: `Deleted user account with user_id = '${user_id}'`, user_id: payload.user_id },
-        transaction
-    );
-    if (!ual) throw new Error(`Failed to delete user [${user_id}]. Unable to create new user activity log`);
+    const ual = await createUserActivityLog({
+        ual_type: 'USER_DELETE',
+        ual_activity: Message.success(['delete', 'user account', user_id]),
+        user_id: payload.user_id
+    }, transaction);
+    if (!ual) throw new Error(Message.failed(['delete', 'user', user_id], {
+        causer: ['create', 'new user activity log']
+    }));
 
     await transaction.commit();
     res.status(200).json(user);
