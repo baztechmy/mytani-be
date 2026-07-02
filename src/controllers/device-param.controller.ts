2fa5b08fd333d16bc7484998b977027433f6f015
Harrypoggers25 <mfa.g4e@gmail.com>
2026-07-03 00:22:09 +0800

    Update: Device param controller and router

--- src/controllers/device-param.controller.ts
+++ src/controllers/device-param.controller.ts
@@ -1,8 +1,8 @@
 // CONFIGS
-import { db, Device, DeviceParam } from "../configs/db.config";
+import { db, DeviceParam } from "../configs/db.config";
 
 // HELPERS
-import { stringifyJson } from "../helpers/json.helper";
+import Message from "../helpers/message.helper";
 
 // MODULES
 import Route from "@harrypoggers25/route";
@@ -13,88 +13,96 @@ import { getPayload } from "../middlewares/authorization.middleware";
 // SERVICES
 import { createUserActivityLog } from "../services/user-activity-log.service";
 
-export const createDeviceParamHandler = Route.asyncHandler(async (req, res) => {
-    const date = new Date();
+export namespace DeviceParamHandler {
+    export const createByDevice = Route.asyncHandler(async (req, res) => {
+        const date = new Date();
 
-    const { dp_did, dp_name, dp_target } = req.body;
-    const [created_at, updated_at] = [date, date];
-    const d_id = +req.params.d_id;
-    const transaction = await db.transaction({ rollbackOnError: true });
+        const d_id = +req.params.d_id;
+        const { dp_did, dp_name } = req.body;
+        const { dp_target } = req.body; // optional
+        const [created_at, updated_at] = [date, date];
+        const transaction = await db.transaction({ rollbackOnError: true });
 
-    const device = await Device.findByPk(d_id);
-    if (!device) throw new Error(`Failed to create new device param ${stringifyJson({ d_id })}. Unable to find device`);
+        const deviceParam = await DeviceParam.create({ dp_did, dp_name, dp_target, created_at, updated_at, d_id }, { transaction });
+        if (!deviceParam) throw new Error(Message.failed(['create', 'new device param', { d_id }]));
 
-    const deviceParam = await DeviceParam.create({ dp_did, dp_name, dp_target, created_at, updated_at, d_id }, { transaction });
-    if (!deviceParam) throw new Error(`Failed to create new device param ${stringifyJson({ d_id })}`);
+        const { dp_id } = deviceParam;
+        const ual = await createUserActivityLog({
+            ual_type: 'DEVICE_PARAM_CREATE',
+            ual_activity: Message.success(['create', 'new device param', dp_id]),
+            user_id: getPayload(req).user_id
+        }, transaction);
+        if (!ual) throw new Error(Message.failed(['create', 'new device param', { d_id }], {
+            causer: ['create', 'new user activity log']
+        }));
 
-    const ual = await createUserActivityLog(
-        { ual_type: 'DEVICE_PARAM_CREATE', ual_activity: `Created new device param with dp_id = '${deviceParam.dp_id}'`, user_id: getPayload(req).user_id },
-        transaction
-    );
-    if (!ual) throw new Error(`Failed to create new device param ${stringifyJson({ d_id })}. Unable to create new user activity log`);
+        await transaction.commit();
+        res.status(201).json(deviceParam);
+    });
 
-    await transaction.commit();
-    res.status(201).json(deviceParam);
-});
+    export const find = Route.asyncHandler(async (req, res) => {
+        const d_id = +req.params.d_id;
+        const dp_id = +req.params.dp_id;
+        const deviceParam = await DeviceParam.findByPk(dp_id, { where: { d_id } });
+        if (!deviceParam) throw new Error(Message.failed(['find', 'device param', dp_id]));
 
-export const findDeviceParamHandler = Route.asyncHandler(async (req, res) => {
-    const dp_id = +req.params.dp_id;
-    const deviceParam = await DeviceParam.findByPk(dp_id);
-    if (!deviceParam) throw new Error(`Failed to find device param [${dp_id}]`);
+        res.status(200).json(deviceParam);
+    });
 
-    res.status(200).json(deviceParam);
-});
+    export const findAllByDevice = Route.asyncHandler(async (req, res) => {
+        const d_id = +req.params.d_id;
+        const deviceParams = await DeviceParam.find({ where: { d_id } });
+        if (!deviceParams) throw new Error(Message.failed(['find', 'device params', { d_id }]));
 
-export const findAllDeviceParamHandler = Route.asyncHandler(async (_, res) => {
-    const deviceParams = await DeviceParam.find({ orderBy: { d_id: 'ASC', dp_id: 'ASC' } });
-    if (!deviceParams) throw new Error('Failed to find all device params');
+        res.status(200).json(deviceParams);
+    });
 
-    res.status(200).json(deviceParams);
-});
+    export const update = Route.asyncHandler(async (req, res) => {
+        const d_id = +req.params.d_id;
+        const dp_id = +req.params.dp_id;
 
-export const findAllDeviceParamByDeviceHandler = Route.asyncHandler(async (req, res) => {
-    const d_id = +req.params.d_id;
-    const deviceParams = await DeviceParam.find({ where: { d_id } });
-    if (!deviceParams) throw new Error(`Failed to find device params ${stringifyJson({ d_id })}`);
+        const { dp_did, dp_name, dp_target } = req.body;
+        const updated_at = new Date();
+        const transaction = await db.transaction({ rollbackOnError: true });
 
-    res.status(200).json(deviceParams);
-});
+        const deviceParam = await DeviceParam.updateByPk(dp_id,
+            { dp_did, dp_name, dp_target, updated_at },
+            { where: { d_id }, transaction }
+        );
+        if (!deviceParam) throw new Error(Message.failed(['update', 'device param', dp_id]));
 
-export const updateDeviceParamHandler = Route.asyncHandler(async (req, res) => {
-    const dp_id = +req.params.dp_id;
-    const { dp_did, dp_name, dp_target, d_id } = req.body;
-    const updated_at = new Date();
-    const transaction = await db.transaction({ rollbackOnError: true });
+        const ual = await createUserActivityLog({
+            ual_type: 'DEVICE_PARAM_UPDATE',
+            ual_activity: Message.success(['update', 'device param', dp_id]),
+            user_id: getPayload(req).user_id
+        }, transaction);
+        if (!ual) throw new Error(Message.failed(['update', 'device param', dp_id], {
+            causer: ['create', 'new user activity log']
+        }));
 
-    const deviceParam = await DeviceParam.updateByPk(dp_id,
-        { dp_did, dp_name, dp_target, updated_at, d_id },
-        { transaction }
-    );
-    if (!deviceParam) throw new Error(`Failed to update device param [${dp_id}]`);
+        await transaction.commit();
+        res.status(200).json(deviceParam);
+    });
 
-    const ual = await createUserActivityLog(
-        { ual_type: 'DEVICE_PARAM_UPDATE', ual_activity: `Updated device param with dp_id = '${dp_id}'`, user_id: getPayload(req).user_id },
-        transaction
-    );
-    if (!ual) throw new Error(`Failed to update device param [${dp_id}]. Unable to create new user activity log`);
+    export const remove = Route.asyncHandler(async (req, res) => {
+        const d_id = +req.params.d_id;
+        const dp_id = +req.params.dp_id;
+        const transaction = await db.transaction({ rollbackOnError: true });
 
-    await transaction.commit();
-    res.status(200).json(deviceParam);
-});
+        const deviceParam = await DeviceParam.deleteByPk(dp_id, { where: { d_id }, transaction });
+        if (!deviceParam) throw new Error(Message.failed(['delete', 'device param', dp_id]));
 
-export const deleteDeviceParamHandler = Route.asyncHandler(async (req, res) => {
-    const dp_id = +req.params.dp_id;
-    const transaction = await db.transaction({ rollbackOnError: true });
+        const ual = await createUserActivityLog({
+            ual_type: 'DEVICE_PARAM_DELETE',
+            ual_activity: Message.success(['delete', 'device', dp_id]),
+            user_id: getPayload(req).user_id
+        }, transaction);
+        if (!ual) throw new Error(Message.failed(['delete', 'device param', dp_id], {
+            causer: ['create', 'new user activity log']
+        }));
 
-    const deviceParam = await DeviceParam.deleteByPk(dp_id, { transaction });
-    if (!deviceParam) throw new Error(`Failed to delete device param [${dp_id}]`);
+        await transaction.commit();
+        res.status(200).json(deviceParam);
+    });
+}
 
-    const ual = await createUserActivityLog(
-        { ual_type: 'DEVICE_PARAM_DELETE', ual_activity: `Deleted device with dp_id = '${dp_id}'`, user_id: getPayload(req).user_id },
-        transaction
-    );
-    if (!ual) throw new Error(`Failed to delete device param [${dp_id}]. Unable to create new user activity log`);
-
-    await transaction.commit();
-    res.status(200).json(deviceParam);
-});
--- src/routers/device.router.ts
+++ src/routers/device.router.ts
@@ -1,7 +1,7 @@
 // CONTROLLERS
 import { DeviceHandler } from '../controllers/device.controller';
 import { DeviceRelayHandler } from '../controllers/device-relay.controller';
-// import { findAllDeviceParamByDeviceHandler } from '../controllers/device-param.controller';
+import { DeviceParamHandler } from '../controllers/device-param.controller';
 
 // MIDDLEWARES
 import AccessControl from '../middlewares/access-control.middleware';
@@ -13,6 +13,7 @@ import { Router } from 'express';
 const deviceRouter = Router();
 deviceRouter.use(Authorize.accesstoken);
 
+// Devices
 deviceRouter.route('/')
     .get(AccessControl.roles(), DeviceHandler.findAll);
 deviceRouter.route('/:d_id')
@@ -20,17 +21,25 @@ deviceRouter.route('/:d_id')
     .patch(AccessControl.deviceOwner(['admin']), DeviceHandler.update)
     .delete(AccessControl.deviceOwner(['admin']), DeviceHandler.remove);
 
+// Device relays
 deviceRouter.route('/:d_id/relays')
     .post(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.createByDevice)
     .get(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.findAllByDevice)
-    .delete(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.removeAllByDevice)
+    .delete(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.removeAllByDevice);
 deviceRouter.route('/:d_id/relays/add')
     .post(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.addByDevice);
 deviceRouter.route('/:d_id/relays/:dr_id')
     .get(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.find)
     .patch(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.update)
-    .delete(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.remove)
-// deviceRouter.route('/:d_id/device-params')
-// .get(AccessControl.deviceOwner(['admin', 'user']), findAllDeviceParamByDeviceHandler);
+    .delete(AccessControl.deviceOwner(['admin', 'user']), DeviceRelayHandler.remove);
+
+// Device params
+deviceRouter.route('/:d_id/params')
+    .post(AccessControl.deviceOwner(['admin', 'user']), DeviceParamHandler.createByDevice)
+    .get(AccessControl.deviceOwner(['admin', 'user']), DeviceParamHandler.findAllByDevice);
+deviceRouter.route('/:d_id/params/:dp_id')
+    .get(AccessControl.deviceOwner(['admin', 'user']), DeviceParamHandler.find)
+    .patch(AccessControl.deviceOwner(['admin', 'user']), DeviceParamHandler.update)
+    .delete(AccessControl.deviceOwner(['admin', 'user']), DeviceParamHandler.remove);
 
 export default deviceRouter;
