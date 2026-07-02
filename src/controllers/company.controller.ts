f00d2dff3c5a0efa7f75a66d20228863411d3c3d
Harrypoggers25 <mfa.g4e@gmail.com>
2026-07-02 17:50:26 +0800

    Update: Implemented Message helper in Company controller

--- src/controllers/company.controller.ts
+++ src/controllers/company.controller.ts
@@ -25,47 +25,43 @@ export const createCompanyHandler = Route.asyncHandler(async (req, res) => {
     let { user_password } = req.body;
     const user_role: Roles = 'admin';
 
-    if (!comp_name) {
-        res.status(400);
-        throw new Error(Message.failed('create', 'new company', { causerMessage: 'comp_name is required' }));
-    }
-
-    if (!user_email) {
-        res.status(400);
-        throw new Error(Message.failed('create', 'new company', { causerMessage: 'user_email is required' }));
-    }
-
-    if (!user_password) {
-        res.status(400);
-        throw new Error(Message.failed('create', 'new company', { causerMessage: 'user_password is required' }));
+    for (const key of ['comp_name', 'user_email', 'user_password']) {
+        if (!req.body[key]) {
+            res.status(400);
+            throw new Error(Message.failed(['create', 'new company'], { subMessage: `${key} is required` }));
+        }
     }
     user_password = hashSync(user_password, 10);
 
     const transaction = await db.transaction({ rollbackOnError: true });
     const company = await Company.create({ comp_name, created_at, updated_at }, { transaction });
-    if (!company) throw new Error(Message.failed('create', 'new company', { causerMessage: 'Internal error' }));
+    if (!company) throw new Error(Message.failed(['create', 'new company']));
 
     const { comp_id } = company;
     const user = await User.create(
         { user_name, user_email, user_phone, user_role, comp_id, created_at, updated_at, created_by },
         { transaction }
     );
-    if (!user) throw new Error(Message.failed('create', 'new company', { causer: ['create', 'admin'] }));
+    if (!user) throw new Error(Message.failed(['create', 'new company'], { causer: ['create', 'admin'] }));
 
     const user_id = user.user_id;
     const userSecret = await UserSecret.create({ user_password, user_id }, { transaction });
-    if (!userSecret) throw new Error(Message.failed('create', 'new company', { causer: ['create', 'admin password'] }));
+    if (!userSecret) throw new Error(Message.failed(['create', 'new company'], { causer: ['create', 'admin password'] }));
 
-    const ual = await createUserActivityLog(
-        {
-            ual_type: 'COMPANY_CREATE',
-            ual_activity: `Created company with comp_id = '${company.comp_id}' and user account with user_id = '${user_id}'`,
-            ual_date: date,
-            user_id: created_by
-        },
-        transaction
-    );
-    if (!ual) throw new Error(Message.failed('create', 'new company', { causer: ['create', 'new user activity log'] }));
+    const ual1 = await createUserActivityLog({
+        ual_type: 'COMPANY_CREATE',
+        ual_activity: Message.success(['create', 'company', comp_id]),
+        ual_date: date,
+        user_id: created_by
+    }, transaction);
+    if (!ual1) throw new Error(Message.failed(['create', 'new company'], { causer: ['create', 'new user activity log'] }));
+    const ual2 = await createUserActivityLog({
+        ual_type: 'USER_CREATE',
+        ual_activity: Message.success(['create', 'user account', user_id]),
+        ual_date: date,
+        user_id: created_by
+    }, transaction);
+    if (!ual2) throw new Error(Message.failed(['create', 'new company'], { causer: ['create', 'new user activity log'] }));
 
     await transaction.commit();
     res.status(201).json({ company, user });
@@ -74,14 +70,14 @@ export const createCompanyHandler = Route.asyncHandler(async (req, res) => {
 export const findCompanyHandler = Route.asyncHandler(async (req, res) => {
     const comp_id = +req.params.comp_id;
     const company = await Company.findByPk(comp_id);
-    if (!company) throw new Error(Message.failed('find', 'company', { where: comp_id }));
+    if (!company) throw new Error(Message.failed(['find', 'company', comp_id]));
 
     res.status(200).json(company);
 });
 
 export const findAllCompanyHandler = Route.asyncHandler(async (_, res) => {
     const companies = await Company.find();
-    if (!companies) throw new Error(Message.failed('find', 'all company'));
+    if (!companies) throw new Error(Message.failed(['find', 'all company']));
 
     res.status(200).json(companies);
 });
@@ -94,14 +90,18 @@ export const updateCompanyHandler = Route.asyncHandler(async (req, res) => {
     const transaction = await db.transaction({ rollbackOnError: true });
 
     const company = await Company.updateByPk(comp_id, { comp_name, updated_at }, { transaction });
-    if (!company) throw new Error(Message.failed('update', 'company', { where: comp_id }));
+    if (!company) throw new Error(Message.failed(['update', 'company', comp_id]));
 
     const payload = getPayload(req);
-    const ual = await createUserActivityLog(
-        { ual_type: 'COMPANY_UPDATE', ual_activity: `Updated company with user_id = '${comp_id}'`, ual_date: updated_at, user_id: payload.user_id },
-        transaction
-    );
-    if (!ual) throw new Error(Message.failed('update', 'company', { where: comp_id, causer: ['create', 'new user activity log'] }));
+    const ual = await createUserActivityLog({
+        ual_type: 'COMPANY_UPDATE',
+        ual_activity: Message.success(['update', 'company', comp_id]),
+        ual_date: updated_at,
+        user_id: payload.user_id
+    }, transaction);
+    if (!ual) throw new Error(Message.failed(['update', 'company', comp_id], {
+        causer: ['create', 'new user activity log']
+    }));
 
     await transaction.commit();
     res.status(200).json(company);
@@ -113,13 +113,16 @@ export const deleteCompanyHandler = Route.asyncHandler(async (req, res) => {
     const transaction = await db.transaction({ rollbackOnError: true });
 
     const company = await Company.deleteByPk(comp_id, { transaction });
-    if (!company) throw new Error(Message.failed('delete', 'company', { where: comp_id }));
+    if (!company) throw new Error(Message.failed(['delete', 'company', comp_id]));
 
-    const ual = await createUserActivityLog(
-        { ual_type: 'COMPANY_DELETE', ual_activity: `Deleted company with user_id = '${comp_id}'`, user_id },
-        transaction
-    );
-    if (!ual) throw new Error(Message.failed('delete', 'company', { where: comp_id, causer: ['create', 'new user activity log'] }));
+    const ual = await createUserActivityLog({
+        ual_type: 'COMPANY_DELETE',
+        ual_activity: Message.success(['delete', 'company', comp_id]),
+        user_id
+    }, transaction);
+    if (!ual) throw new Error(Message.failed(['delete', 'company', comp_id], {
+        causer: ['create', 'new user activity log']
+    }));
 
     await transaction.commit();
     res.status(200).json(company);
