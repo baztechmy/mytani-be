6e76417558ee8e7f8a60106fa2dec17f70f749eb
Harrypoggers25 <mfa.g4e@gmail.com>
2026-07-09 17:37:54 +0800

    Add: Mqtt config

---
+++ src/configs/mqtt.config.ts
@@ -0,0 +1,11 @@
+import Mqtt from "../helpers/mqtt.helper";
+import env from "./env.config";
+
+export const mqttClient = Mqtt.define({
+    host: env.MQTT_HOST,
+    username: env.MQTT_USER,
+    password: env.MQTT_PASSWORD,
+    port: env.MQTT_PORT,
+    protocol: 'mqtt',
+});
+
